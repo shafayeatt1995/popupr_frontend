19301a9813cbb7c4a859b02bd1feed49fb2e6456
shafayeatt1995 <shafayetalanik@gmail.com>
2024-11-11 06:34:27 +0600

    ui updated

--- components/home/GenerateUrl.jsx
+++ components/home/GenerateUrl.jsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "../ui/button";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Loader2Icon } from "lucide-react";
 import { userApi } from "@/server";
 import { authUser } from "@/services/nextAuth";
 import { useRouter } from "next/navigation";
@@ -10,33 +10,47 @@ import { toast } from "sonner";
 
 export default function GenerateUrl({ productName }) {
   const router = useRouter();
-  const generateUrl = async () => {
-    const user = await authUser();
-    if (user) {
+  const [loading, setLoading] = useState(false);
+  const click = useRef(true);
+
+  const openUrl = async () => {
+    if (click.current) {
       try {
-        const { priceID, discountID } = await userApi.generatePaymentUrl({
-          productName,
-        });
-        if (priceID) {
-          await openCheckout({
-            priceId: priceID,
-            discountId: discountID,
-            email: user.email,
-            customData: { user },
-          });
+        click.current = false;
+        setLoading(true);
+        const user = await authUser();
+        if (user) {
+          try {
+            const { priceID, discountID } = await userApi.generatePaymentUrl({
+              productName,
+            });
+            if (priceID) {
+              await openCheckout({
+                priceId: priceID,
+                discountId: discountID,
+                email: user.email,
+                customData: { user },
+              });
+            }
+          } catch (error) {
+            if (error?.error?.toast) {
+              toast.error(error.error.toast);
+            }
+          }
+        } else {
+          router.push("/login");
         }
       } catch (error) {
-        if (error?.error?.toast) {
-          toast.error(error.error.toast);
-        }
+      } finally {
+        click.current = true;
+        setLoading(false);
       }
-    } else {
-      router.push("/login");
     }
   };
 
   return (
-    <Button className="w-full mt-10" onClick={generateUrl}>
+    <Button className="w-full mt-10" onClick={openUrl} disabled={loading}>
+      {loading && <Loader2Icon className="animate-spin" />}
       Get Popupr <ChevronRightIcon />
     </Button>
   );
