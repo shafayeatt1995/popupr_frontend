14acca6d7de18c208bbf052783a71cc320b2781e
shafayeatt1995 <shafayetalanik@gmail.com>
2024-11-30 15:15:45 +0600

    Script updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,120 @@
-!function(){let e;const t=(e=>"www."===e.slice(0,4)?e.slice(4):e);const n=(e,t)=>{if(t>1)document.getElementById(`popupr-message-${t-1}`).classList.replace("animate-slideIn","animate-appearFromRight");const o=document.createElement("div");o.className=`popupr-message animate-slideIn`,o.id=`popupr-message-${t}`,o.innerHTML=`<div class="popupr-message-image"><img src="${e.image}" alt="${e.title}"></div><div class="popupr-message-content"><h2>${e.title}</h2><p>${e.message}</p></div><div class="popupr-times"><p class="text-sm text-gray-600">${e.time}</p></div>`,e.appendChild(o)};const o=()=>{e.firstElementChild?.remove()};const r=({hide:e,messages:t,send:n,start:o})=>{const s=[...t].reverse(),i=document.createElement("div");i.classList.add("popupr-container"),e=document.createElement("div");e.classList.add("popupr-messages"),i.appendChild(e),document.body.appendChild(i),window.addEventListener("scroll",()=>{500<window.scrollY&&(i.style.transition="opacity 300ms",i.style.opacity="0",setTimeout(()=>{i.remove(),window.removeEventListener("scroll",()=>{})},500))}),const a=()=>{let s=0;const l=setInterval(()=>{t.length>s&&(n(t[s],++s),setTimeout(o,hide)),t.length<=s&&clearInterval(l)},n)},c=()=>{const l=(s=0)=>{n(t[s],s),setTimeout(()=>{o(),setTimeout(()=>l(s+1),100)},hide)};l()};fetch("https://popupr.com/css/style.css").then(e=>e.text()).then(e=>{const t=document.createElement("style");t.innerHTML=e,document.head.appendChild(t)}),setTimeout(()=>{768>=window.innerWidth?c():a()},o)};window.addEventListener("load",async()=>{try{const e=document.currentScript?.getAttribute("data-domain")??document.querySelector("script[data-domain]")?.getAttribute("data-domain");const t=`https://api.popupr.com/domain-messages?domain=${e}`,o=await fetch(t),s=await o.json();s&&s.messages.length>0&&r(s)}catch(e){}})};
+(function () {
+  let a;
+  const cleanDomain = (domain) =>
+    domain.startsWith("www.") ? domain.slice(4) : domain;
+
+  const b = async () => {
+    try {
+      const c =
+        document.currentScript?.getAttribute("data-domain") ??
+        document
+          .querySelector("script[data-domain]")
+          ?.getAttribute("data-domain");
+
+      const locationHostname =
+        cleanDomain(location.hostname) +
+        (location.pathname.length > 1 ? location.pathname : "");
+      if (c === locationHostname) {
+        const d = `https://api.popupr.com/domain-messages?domain=${c}`;
+        const e = await fetch(d);
+        const { dd } = await e.json();
+        if (dd) {
+          if (dd && dd.messages.length > 0) h(dd);
+        }
+      }
+    } catch (err) {}
+  };
+
+  const i = async () => {
+    const j = "https://popupr.com/css/style.css";
+    const k = await fetch(j);
+    const l = await k.text();
+    const m = document.createElement("style");
+    m.innerHTML = l;
+    document.head.appendChild(m);
+  };
+
+  const n = (o, p) => {
+    if (p > 1) {
+      document
+        .getElementById(`popupr-message-${p - 1}`)
+        ?.classList.replace("animate-slideIn", "animate-appearFromRight");
+    }
+    const q = document.createElement("div");
+    q.className = `popupr-message animate-slideIn`;
+    q.id = `popupr-message-${p}`;
+    q.innerHTML = `
+      <div class="popupr-message-image">
+        <img src="${o.image}" alt="${o.title}">
+      </div>
+      <div class="popupr-message-content">
+        <h2>${o.title}</h2>
+        <p>${o.message}</p>
+      </div>
+      <div class="popupr-times">
+        <p class="text-sm text-gray-600">${o.time}</p>
+      </div>
+    `;
+    a.appendChild(q);
+  };
+
+  const r = () => {
+    a.firstElementChild?.remove();
+  };
+
+  const h = ({ hide, messages, send, start }) => {
+    const o = [...messages].reverse();
+    const s = document.createElement("div");
+    s.classList.add("popupr-container");
+    a = document.createElement("div");
+    a.classList.add("popupr-messages");
+    s.appendChild(a);
+    const t = () => {
+      let u = 0;
+      const v = setInterval(() => {
+        if (u < o.length) {
+          const w = o[u++];
+          n(w, u);
+          setTimeout(() => {
+            r();
+          }, hide);
+        } else {
+          clearInterval(v);
+        }
+      }, send);
+    };
+    const x = () => {
+      const y = (z = 0) => {
+        n(o[z], z);
+        setTimeout(() => {
+          r();
+          setTimeout(() => y(z + 1), 100);
+        }, hide);
+      };
+      y();
+    };
+
+    i();
+    setTimeout(() => {
+      if (window.innerWidth <= 768) {
+        x();
+      } else {
+        t();
+      }
+    }, start);
+    document.body.appendChild(s);
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 500) {
+        document.querySelector(".popupr-container")?.style.transition =
+          "opacity 300ms";
+        document.querySelector(".popupr-container")?.style.opacity = "0";
+        setTimeout(() => {
+          document.querySelector(".popupr-container")?.remove();
+          window.removeEventListener("scroll", () => {});
+        }, 500);
+      }
+    });
+  };
+
+  window.addEventListener("load", b);
+})();
